4557cc2e275392e3c947010fd820336424849090
kunal98singh <singh.kunal9800@gmail.com>
2025-05-27 18:08:35 -0400

    New Solution

---
+++ src/SolutionDSA/Valid_Anagram.java
@@ -0,0 +1,21 @@
+package SolutionDSA;
+
+import java.util.Arrays;
+
+public class Valid_Anagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        return Arrays.equals(sSort, tSort);
+        
+
+
+    }
+    
+}
