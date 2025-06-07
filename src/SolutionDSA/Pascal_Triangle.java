0e34f96b1a72c32e183eb80801a0ca7d5bb7a2cb
kunal98singh <singh.kunal9800@gmail.com>
2025-06-07 13:20:14 -0400

    Solution Pascal Triangle

---
+++ src/SolutionDSA/Pascal_Triangle.java
@@ -0,0 +1,30 @@
+package SolutionDSA;
+// Pascal's Triangle
+// https://leetcode.com/problems/pascals-triangle/
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pascal_Triangle {
+     public List<List<Integer>> generate(int numRows) {
+      
+
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int row = 0; row < numRows; row++) {
+            List<Integer> currentRow = new ArrayList<>();
+            
+            for (int col = 0; col <= row; col++) {
+                if (col == 0 || col == row) {
+                    currentRow.add(1); 
+                } else {
+                    int val = triangle.get(row - 1).get(col - 1) + triangle.get(row - 1).get(col);
+                    currentRow.add(val); 
+                }
+            }
+            triangle.add(currentRow);
+        }
+
+        return triangle;
+
+    }
+}
